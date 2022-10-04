7353b294852600d8d404d4496d66681c7fbbb7f0
Jorge Vargas <jorge.vargas@zemoga.com>
2022-10-04 15:39:14 -0500

    feat: add container intro page

--- packages/client/src/pages/introduction/IntroductionPage.js
+++ packages/client/src/pages/introduction/IntroductionPage.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import logo from '../../assets/images/logo.png';
+import './IntroductionPage.css';
 
 export default function IntroductionPage() {
-  return <div>IntroductionPage</div>;
+  return (
+    <div className="introduction_page_container">
+      <div className="introduction_page_panel">
+        <img className="introduction_page_image" src={logo} alt="logo" />
+        Buttons
+      </div>
+    </div>
+  );
 }
