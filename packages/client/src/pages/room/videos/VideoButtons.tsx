2e047d7e328bbb2af48935b6ee88d34ff6cef152
Jorge Vargas <jorge.vargas@zemoga.com>
2022-11-11 09:09:24 -0500

    feat: hide video button for only audio conn

--- packages/client/src/pages/room/videos/VideoButtons.tsx
+++ packages/client/src/pages/room/videos/VideoButtons.tsx
@@ -1,18 +1,24 @@
+import { pick } from 'lodash-es';
+import { useAppSelector } from '../../../hooks';
 import CameraButton from './buttons/CameraButton';
 import LeaveRoomButton from './buttons/LeaveRoomButton';
 import MicButton from './buttons/MicButton';
 import SwitchToScreenSharingButton from './buttons/SwitchToScreenSharingButton';
 
 export default function VideoButtons() {
+  const { connectOnlyWithAudio } = useAppSelector((state) =>
+    pick(state.room, ['connectOnlyWithAudio'])
+  );
+
   return (
     <div className="video_buttons_container">
       <MicButton />
 
-      <CameraButton />
+      {!connectOnlyWithAudio && <CameraButton />}
 
       <LeaveRoomButton />
 
-      <SwitchToScreenSharingButton />
+      {!connectOnlyWithAudio && <SwitchToScreenSharingButton />}
     </div>
   );
 }
